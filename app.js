7c416fb04f987f2837db56d253145570b41a0804
anuragkrishna <anurag.bits18@gmail.com>
2017-03-21 17:08:15 +0530

    ff fix cookie not set

--- app.js
+++ app.js
@@ -43,15 +43,13 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    cookie: { secure:false,maxAge: 180 * 60 * 1000 }
+    cookie: { maxAge: 180 * 60 * 1000 }
 }));
 
 app.use(function(req, res, next) {
  
-  res.header("Access-Control-Allow-Origin", "https://buycle.herokuapp.com:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Credentials", "true");
 
   next();
 });
