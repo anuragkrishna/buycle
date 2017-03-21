bde3750dc840a409ca3a12e38c310520247b0a6e
anuragkrishna <anurag.bits18@gmail.com>
2017-03-21 17:13:56 +0530

    ff fix cookie not set

--- app.js
+++ app.js
@@ -43,16 +43,13 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    cookie: { maxAge: 180 * 60 * 1000 }
+    cookie: { secure:false, maxAge: 180 * 60 * 1000 }
 }));
 
 app.use(function(req, res, next) {
  
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Credentials", "true");
-
   next();
 });
 
